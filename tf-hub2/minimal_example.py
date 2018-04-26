04e9626d628f868d7a0ae57eae292bf2834c949b
deco <deco@cubee.com>
2018-04-26 15:42:05 +0800

    Add sentence_embedding.py

--- tf-hub2/minimal_example.py
+++ tf-hub2/minimal_example.py
@@ -6,7 +6,15 @@
 @Desc      : 
 """
 
+import tensorflow as tf
 import tensorflow_hub as hub
+import time
+import os
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+
+start_time = time.time()
+# print(start_time)
 
 embed = hub.Module("https://tfhub.dev/google/"
                    "universal-sentence-encoder/1")
@@ -15,3 +23,14 @@ if __name__ == '__main__':
     embedding = embed([
         "The quick brown fox jumps over the lazy dog."])
     print(embedding)
+    print(type(embed))
+
+    sess = tf.Session()
+    saver = tf.train.Saver()
+    sess.run(tf.global_variables_initializer())
+    saver.save(sess, os.path.join(file_dir, 'data/universal-sentence-encoder'))
+
+    embed.export(os.path.join(file_dir, 'data/universal-sentence-encoder'),
+                 sess)
+
+    print(time.time()-start_time)
---
+++ tf-hub2/sentence_embedding.py
@@ -0,0 +1,34 @@
+"""
+@Project   : DuReader
+@Module    : sentence_embedding.py
+@Author    : Deco [deco@cubee.com]
+@Created   : 4/26/18 2:46 PM
+@Desc      : 
+"""
+import tensorflow as tf
+import tensorflow_hub as hub
+import os
+
+if __name__ == '__main__':
+    file_dir = os.path.dirname(os.path.abspath(__file__))
+    # sess = tf.Session()
+    # saver = tf.train.Saver()
+    # sess.run(tf.global_variables_initializer())
+    # saver.restore(sess,
+    #               os.path.join(file_dir, 'data/universal-sentence-encoder'))
+    # https://stackoverflow.com/questions/34982492/restoring-tensorflow-model
+
+    embed = hub.Module(os.path.join(file_dir,
+                                    'data/universal-sentence-encoder'))
+
+    embedding = embed([
+        "The quick brown fox jumps over the lazy dog.",
+        "Who is Messy"])
+
+    with tf.Session() as session:
+        session.run(
+            [tf.global_variables_initializer(), tf.tables_initializer()])
+        message_embedding = session.run(embedding)
+
+    print(message_embedding)
+    print(message_embedding.shape)
