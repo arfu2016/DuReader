fad3cc5b3378de4faae660018190b04ed2c11536
deco <deco@cubee.com>
2018-07-02 18:31:49 +0800

    Update tf-hub2/sentence_similarity/sen_sim.py

--- tf-hub2/sentence_similarity/sen_sim.py
+++ tf-hub2/sentence_similarity/sen_sim.py
@@ -11,6 +11,7 @@ import os
 import numpy as np
 from cachetools import cached, TTLCache
 import time
+from threading import RLock
 
 # embed = hub.Module("https://tfhub.dev/google/"
 #                    "universal-sentence-encoder/1")
@@ -23,51 +24,72 @@ cache = TTLCache(maxsize=100, ttl=300)
 # If no expired items are there to remove, the least recently used items will
 # be discarded first to make space when necessary.
 
+lock = RLock()
+
 
 @cached(cache)
-def sentence_embedding(sentences: tuple) -> np.ndarray:
+def _sentence_embedding(sentences: tuple) -> np.ndarray:
     embedding = tf.nn.l2_normalize(embed(sentences))
 
     with tf.Session() as session:
         session.run(
             [tf.global_variables_initializer(), tf.tables_initializer()])
         sen_embedding = session.run(embedding)
+
+    # time.sleep(0.1)
     return sen_embedding
 
 
-def test_sentence_embedding():
+def _produce_sentence_embedding():
     training_sentences = ("The quick brown fox jumps over the lazy dog.",
                           "Who is Messy")
-    training_embeddings = sentence_embedding(training_sentences)
+    training_embeddings = _sentence_embedding(training_sentences)
     # print('type of training_embeddings:')
     # print(type(training_embeddings))
 
     test_sentence = ('Can you tell me something about Messy',)
-    test_embedding = sentence_embedding(test_sentence)
+    test_embedding = _sentence_embedding(test_sentence)
 
     # print('Embeddings of training sentences:')
     # print(training_embeddings)
     # print('Embeddings of test sentence:')
     # print(test_embedding)
-
     return training_embeddings, test_embedding
 
 
+def test_sentence_embedding():
+    print()
+    print('Test test_sentence_embedding()')
+    training_embeddings, test_embedding = _produce_sentence_embedding()
+
+    print('Shape of embeddings of training sentences:')
+    print(training_embeddings.shape)
+    print('Shape of embeddings of test sentence:')
+    print(test_embedding.shape)
+
+
 def test_cache():
+    print()
+    print('Test test_cache()')
+    with lock:
+        cache.clear()
+    # synchronization, 使用lock主要为了应对多线程情况
+    # [clear the cache](http://cachetools.readthedocs.io/en/latest/)
+    # 多进程实现的话一般是异步实现？
     start = time.perf_counter()
-    test_sentence_embedding()
+    _produce_sentence_embedding()
     print('time without cache:', time.perf_counter()-start)
     start = time.perf_counter()
-    test_sentence_embedding()
-    print('time after cache:', time.perf_counter() - start)
+    _produce_sentence_embedding()
+    print('time with cache:', time.perf_counter() - start)
 
 
-def vector_similarity(encode1: list, encode2: list) -> float:
+def _vector_similarity(encode1: list, encode2: list) -> float:
 
-    sim_scores = np.sum(np.multiply(encode1, encode2))
+    sim_score = np.sum(np.multiply(encode1, encode2))
     # realization of dot product of two vectors
 
-    return sim_scores
+    return sim_score
 
 
 def similarity_scores(training_vectors: np.ndarray,
@@ -77,17 +99,23 @@ def similarity_scores(training_vectors: np.ndarray,
     test_vector = test_vector.tolist()
     test_vector = test_vector[0]
 
-    sim_scores = [vector_similarity(training_vector, test_vector)
+    sim_scores = [_vector_similarity(training_vector, test_vector)
                   for training_vector in training_vectors]
 
     return sim_scores
 
 
 def test_similarity_scores():
-    training_embeddings, test_embedding = test_sentence_embedding()
+    print()
+    print('Test test_similarity_scores()')
+    training_embeddings, test_embedding = _produce_sentence_embedding()
     sim_scores = similarity_scores(training_embeddings, test_embedding)
     print('Similarity scores:')
     print(sim_scores)
+
+
+def most_similar(training_sentences: list, test_sentence: list) -> str:
+    pass
 
 
 if __name__ == '__main__':
