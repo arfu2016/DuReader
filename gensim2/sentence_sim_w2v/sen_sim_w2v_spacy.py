04bc958d2763c4c8d624ca0025182d7e4f84882a
deco <deco@cubee.com>
2018-07-09 12:15:04 +0800

    Update gensim2

--- gensim2/sentence_sim_w2v/sen_sim_w2v_spacy.py
+++ gensim2/sentence_sim_w2v/sen_sim_w2v_spacy.py
@@ -1,30 +1,22 @@
 """
 @Project   : DuReader
-@Module    : sen_sim_w2v.py
+@Module    : sen_sim_w2v_spacy.py
 @Author    : Deco [deco@cubee.com]
-@Created   : 7/6/18 10:10 AM
-@Desc      : 给出一组句子，找到其中和一个另给的句子最相似的句子
-句向量用word2vec baseline来算
+@Created   : 7/9/18 11:18 AM
+@Desc      : 
 """
+import logging
 import os
+import string
 import time
 from multiprocessing import Pool
-import logging
-import string
 
 import numpy as np
-# import tensorflow as tf
-# import tensorflow_hub as hub
+import spacy
 from cachetools import cached, TTLCache
-from nltk.tokenize import word_tokenize
-from gensim.models import KeyedVectors
 
 file_dir = os.path.dirname(os.path.dirname(__file__))
-# embed = hub.Module(os.path.join(file_dir,
-#                                 'data/universal-sentence-encoder'))
 cache = TTLCache(maxsize=100, ttl=300)
-# session = tf.Session()
-# session.run([tf.global_variables_initializer(), tf.tables_initializer()])
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
                     level=logging.INFO)
 
@@ -32,49 +24,31 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s',
 # 把句子转换为向量
 
 def model_load():
-    fname = os.path.join(file_dir,
-                         'data/GoogleNews-vectors-negative300.bin.gz')
-    model0 = KeyedVectors.load_word2vec_format(fname, binary=True)
+    nlp0 = spacy.load('en_core_web_md')
     logging.info('The model was loaded.')
-    return model0
+    return nlp0
 
 
-def init_model():
-    model0 = model_load()
-    vocab_dict0 = model0.wv.vocab
+nlp = model_load()
+
+
+def init_punctuation():
     punc_string = string.punctuation + '。，“”‘’（）：；？·—《》、'
     punc_set0 = {punc for punc in punc_string}
-    return model0, vocab_dict0, punc_set0
+    return punc_set0
 
 
-model, vocab_dict, punc_set = init_model()
-
-
-def _get_word_vector(word):
-    return model.wv[word]
+punc_set = init_punctuation()
 
 
 @cached(cache)
 def _single_sentence(sentence: str) -> list:
-    word_list = word_tokenize(sentence)
-    # print(sentence)
-    # print(word_list)
-    # print()
+    word_list = nlp(sentence)
     word_list = [word for word in word_list if word not in punc_set]
-
-    # word_list = [word for word in word_list if word in vocab_dict]
-    # with Pool(2) as p:
-    #     word_vectors = p.map_async(_get_word_vector, word_list)
-
-    # start = time.perf_counter()
-    word_vectors = [model.wv[word] for word in word_list
-                    if word in vocab_dict]
-    # print('Time to get vectors:', time.perf_counter()-start)
-    # word_vectors = [model.wv[word].tolist() for word in word_list
-    #                 if word in vocab_dict]
+    word_vectors = [word.vector for word in word_list
+                    if word.has_vector]
     st_matrix = np.array(word_vectors)
     st_vector = np.mean(st_matrix, axis=0)
-    # st_vector = np.mean(st_matrix, axis=0).tolist()
     st_vector = st_vector / np.linalg.norm(st_vector)
     st_vector = st_vector.tolist()
 
@@ -87,9 +61,6 @@ def _sentence_embedding(sentences: tuple) -> np.ndarray:
     with Pool(2) as p:
         sen_embedding = p.map(_single_sentence, sentences)
     sen_embedding = np.array(sen_embedding)
-    # embedding = tf.nn.l2_normalize(embed(sentences))
-    # sen_embedding = session.run(embedding)
-
     return sen_embedding
 
 
@@ -130,9 +101,6 @@ def _similarity_scores(training_vectors: np.ndarray,
 
     sim_scores = [_vector_similarity(vector, test_vector)
                   for vector in training_vectors]
-    # with Pool(2) as p:
-    #     sim_scores = p.map(VectorSimilarity(test_vector),
-    #                        training_vectors)
 
     return sim_scores
 
@@ -140,16 +108,10 @@ def _similarity_scores(training_vectors: np.ndarray,
 # 给定一个句子和一组句子，找出后者各个句子中和前者最为相似的句子
 
 def most_similar(training_sentences: tuple, test_sentence: str) -> str:
-    # start = time.perf_counter()
     training_embeddings = _sentence_embedding(training_sentences)
-    # print('time to get training_embeddings:', time.perf_counter()-start)
-    # start = time.perf_counter()
     test_embedding = _single_sentence(test_sentence)
-    # print('time to get test_embedding:', time.perf_counter() - start)
-    # start = time.perf_counter()
     sim_scores = _similarity_scores(training_embeddings, test_embedding)
     # print('sim_scores:', sim_scores)
-    # print('time to get similarity_scores:', time.perf_counter() - start)
     idx = np.argmax(sim_scores)
     return training_sentences[idx]
 
@@ -188,5 +150,3 @@ if __name__ == '__main__':
 
     t_most_similar()
     t2_most_similar()
-
-    # session.close()
