94339371f848048d4a1e3c58683032036d4bd7b0
deco <deco@cubee.com>
2018-08-07 17:06:38 +0800

    Update tools/trim_json.py

--- tools/trim_json.py
+++ tools/trim_json.py
@@ -8,7 +8,19 @@
 import json
 
 file_name = '/decaNLP/.data/squad/train-v1.1.json'
+train_file = '/home/projects/DuReader/data/demo_squad/train_demo.json'
+valid_file = '/home/projects/DuReader/data/demo_squad/valid_demo.json'
 
+train_data = dict()
+valid_data = dict()
 with open(file_name, encoding='utf-8') as fin:
     squad = json.load(fin)['data']
     print(type(squad))
+    train_data['data'] = squad[0:100]
+    valid_data['data'] = squad[100:200]
+
+with open(train_file, 'w', encoding='utf-8') as fout:
+    json.dump(train_data, fout)
+
+with open(valid_file, 'w', encoding='utf-8') as fout:
+    json.dump(valid_data, fout)
