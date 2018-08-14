0b0bc16019ccff33c159c6300c46a3c95d03cda6
deco <deco@cubee.com>
2018-08-14 19:02:53 +0800

    Update work4/

---
+++ work4/__init__.py
@@ -0,0 +1,7 @@
+"""
+@Project   : DuReader
+@Module    : __init__.py.py
+@Author    : Deco [deco@cubee.com]
+@Created   : 8/14/18 5:06 PM
+@Desc      : 
+"""
---
+++ work4/scrapy2/dongqiudi/__init__.py
@@ -0,0 +1,11 @@
+"""
+@Project   : DuReader
+@Module    : __init__.py
+@Author    : Deco [deco@cubee.com]
+@Created   : 8/14/18 5:16 PM
+@Desc      :
+scrapy check basic
+scrapy crawl basic -o items.json
+
+scrapy crawl manual -o items.json
+"""
