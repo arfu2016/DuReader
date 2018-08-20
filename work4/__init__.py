2eaebc6994d256ef92a91e09cb7372a3445d5f9f
deco <deco@cubee.com>
2018-08-20 11:21:39 +0800

    Update work3 and work4

--- work4/__init__.py
+++ work4/__init__.py
@@ -3,5 +3,14 @@
 @Module    : __init__.py.py
 @Author    : Deco [deco@cubee.com]
 @Created   : 8/14/18 5:06 PM
-@Desc      : 
+@Desc      :
+1. cd scrapy2/dongqiudi
+python data_preprocessing/clear_items.py
+python data_preprocessing/show_items.py
+scrapy crawl manual -o items.json
+2. cd elasticsearch2
+python add_dongqiudi.py
+python extract_person.py
+3. cd similar_sentence
+python similar_info.py
 """
